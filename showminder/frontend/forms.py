78c77c769c1774b5c69b42e84731413041b53c35
Christian Assing <chris@ca-net.org>
2017-01-15 15:05:30 +0100

    store more imdb attributes in db

--- showminder/frontend/forms.py
+++ showminder/frontend/forms.py
@@ -7,15 +7,13 @@ class ImdbIdForm(forms.Form):  # noqa
     imdb_id = forms.CharField(label="Imdb ID")
     season = forms.IntegerField(initial=1, min_value=1)
     episode = forms.IntegerField(initial=1, min_value=1)
-    last_seen = forms.DateField()
 
     def clean(self):
         imdb_id = self.cleaned_data['imdb_id']
         season = self.cleaned_data['season']
         episode = self.cleaned_data['episode']
-        last_seen = self.cleaned_data['last_seen']
         try:
-            self._tvshow = TvShow.from_imdb(imdb_id, season, episode, last_seen)
+            self._tvshow = TvShow.from_imdb(imdb_id, season, episode)
         except:
             raise forms.ValidationError("Imdb ID not found!")
 
--- showminder/frontend/models.py
+++ showminder/frontend/models.py
@@ -1,18 +1,31 @@
+
+from datetime import date
 from django.db import models
 from imdbpie import Imdb
 
 imdb = Imdb(anonymize=True)
 
 
-class TvShow(models.Model):
-    """TV Show."""
+def unix0():
+    return date.fromtimestamp(0)
 
-    title = models.TextField()
-    cover_url = models.URLField()
+
+class TvShow(models.Model):
+    """TV Show (Movie or Series)."""
+
+    title = models.CharField(max_length=255)
+    imdb_id = models.CharField(max_length=20, null=True)
+    cover_url = models.URLField(max_length=2023)
+    trailer_url = models.URLField(max_length=2023, null=True)
     rating = models.FloatField()
+    genres = models.CharField(max_length=255, null=True)
+    tagline = models.TextField(default="No tagline.", blank=True)
+    release_date = models.DateField(null=True)
+    typ = models.CharField(default='tv_series', max_length=30)
+    # my attributes
+    last_seen = models.DateField(default=unix0)
     season = models.IntegerField(default=1)
     episode = models.IntegerField(default=1)
-    last_seen = models.DateField()
 
     class Meta:  # noqa
         ordering = ['-last_seen', 'title']
@@ -21,14 +34,19 @@ class TvShow(models.Model):
         return self.title
 
     @classmethod
-    def from_imdb(cls, imdb_id, season, episode, last_seen):
+    def from_imdb(cls, imdb_id, season=0, episode=0):
         item = imdb.get_title_by_id(imdb_id)
         tvshow = cls(
             title=item.title,
+            imdb_id=imdb_id,
             cover_url=item.cover_url,
+            trailer_url=item.trailers[0]['url'] if item.trailers else None,
             rating=item.rating,
+            genres=", ".join(item.genres),
+            tagline=item.tagline if item.tagline else "",
+            release_date=item.release_date,
+            typ=item.type,
             season=season,
             episode=episode,
-            last_seen=last_seen,
         )
         return tvshow
