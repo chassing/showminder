2ad3fa0a766a8f4021b11b2d9d2eac6cfe3ee52a
Christian Assing <chris@ca-net.org>
2017-01-15 16:38:32 +0100

    backup db task

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import date
 from fabric.api import env
 from fabric.api import local
 from fabric.api import run
@@ -15,3 +16,11 @@ def build():
     # build
     local(f"docker build -t showminder/showminder:{VERSION} -t showminder/showminder:latest .")
     run(f'docker service update --image showminder/showminder:{VERSION} showminder')
+
+
+
+@task
+def backup():
+    """Create a prod database backup."""
+    today = date.today().strftime("%Y-%m-%d")
+    local(f"pg_dump --host=brain.local --username=showminder --file=showminder_{today}.sql showminder")
