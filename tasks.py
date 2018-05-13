0f7728ff60aaa806843f8bc54fc4a72627ff3ed4
Christian Assing <chris@ca-net.org>
2018-05-13 15:55:02 +0200

    RIP fabric

---
+++ tasks.py
@@ -0,0 +1,22 @@
+
+
+from datetime import datetime as dt
+from invoke import task
+
+VERSION = '0.22'
+
+SRV = 'core@brain'
+
+
+@task
+def build(c):
+    """Build and deploy new version."""
+    c.run(f"docker build -t showminder/showminder:{VERSION} -t showminder/showminder:latest .")
+    c.run(f'ssh {SRV} docker service update --image showminder/showminder:{VERSION} showminder')
+
+
+@task
+def backup(c):
+    """Create a backup of prod database."""
+    c.run(f"pg_dump --host=brain.local --username=showminder --file=backup/showminder.{dt.now():%Y-%m-%d}.sql showminder")
+    c.run(f"ls -lrt backup")
